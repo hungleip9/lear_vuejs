f7504378ce7ed21fc1d4e5987164b92a344cda57
hungleip9 <hungleip9@gmail.com>
2021-01-05 10:39:23 +0700

    up bai 3

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,22 @@
 <!--    <img alt="Vue logo" src="./assets/logo.png">-->
     <Bai1_2 />
     <Bai1_1 />
+    <Bai1_3 />
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 // import Bai1_2 from './components/Bai1_2.vue'
-import Bai1_1 from './components/Bai1_1.vue'
+// import Bai1_1 from './components/Bai1_1.vue'
+import Bai1_3 from './components/Bai1_3.vue'
 
 export default {
   name: 'App',
   components: {
     // Bai1_2,
-    Bai1_1,
+    // Bai1_1,
+    Bai1_3,
   }
 }
 </script>
---
+++ src/components/Bai1_3.vue
@@ -0,0 +1,167 @@
+<template>
+  <div class="container">
+    <table>
+      <tr>
+        <td colspan="4">
+          <div class="display"><p>{{content || '0'}}</p></div>
+        </td>
+      </tr>
+      <tr>
+        <td><input type="button" value="C" @click="clear"></td>
+        <td><input type="button" value="DEL"></td>
+        <td><input type="button" value="%" @click="percent"></td>
+        <td><input class="btnyellow" type="button" value="/" @click="divide"></td>
+      </tr>
+      <tr>
+        <td><input @click="append('7')" type="button" name="one" value="7"></td>
+        <td><input @click="append('8')" type="button" name="two" value="8"></td>
+        <td><input @click="append('9')" type="button" name="three" value="9"></td>
+        <td><input class="btnyellow" type="button" name="plus" @click="times" value="*"></td>
+      </tr>
+      <tr>
+        <td><input @click="append('4')" type="button" name="four" value="4"></td>
+        <td><input @click="append('5')" type="button" name="five" value="5"></td>
+        <td><input @click="append('6')" type="button" name="six" value="6"></td>
+        <td><input class="btnyellow" @click="minus" type="button" name="minus" value="-"></td>
+      </tr>
+      <tr>
+        <td><input @click="append('1')" type="button" name="seven" value="1"></td>
+        <td><input @click="append('2')" type="button" name="eight" value="2"></td>
+        <td><input @click="append('3')" type="button" name="nine" value="3"></td>
+        <td><input class="btnyellow" @click="add" type="button" name="times" value="x"></td>
+      </tr>
+      <tr>
+        <td colspan="2"><input @click="append('0')" type="button" class="operator btnzero" name="zero" value="0"></td>
+        <td><input @click="dot" type="button" class="operator" name="div" value="."></td>
+        <td><input @click="equal" class="btnyellow" type="button" name="doit" value="="></td>
+
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+  data (){
+    return{
+      content: '600',
+      operator: null,
+      operatorClicked: false,
+      previous: null,
+
+    }
+  },
+  methods: {
+    // changeUserName (value){
+    //   this.name = value
+    // },
+    clear(){
+      this.content= '';
+    },
+    append(number){
+      if (this.operatorClicked) {
+        this.content = '';
+        this.operatorClicked = false;
+      }
+      this.content = `${this.content}${number}`;
+    },
+    percent(){
+      this.content = `${parseFloat(this.content) / 100}`;
+    },
+    dot() {
+      if (this.content.indexOf('.') === -1) {
+        this.append('.');
+      }
+    },
+    setPrevious() {
+      this.previous = this.content;
+      this.operatorClicked = true;
+    },
+    divide(){
+      this.operator = (a, b) => a / b;
+      this.setPrevious();
+    },
+    times(){
+      this.operator = (a, b) => a * b;
+      this.setPrevious();
+    },
+    minus() {
+      this.operator = (a, b) => a - b;
+      this.setPrevious();
+    },
+    add() {
+      this.operator = (a, b) => a + b;
+      this.setPrevious();
+    },
+    equal() {
+      this.content = `${this.operator(
+          parseFloat(this.content),
+          parseFloat(this.previous)
+      )}`;
+      this.previous = null;
+    }
+
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+//@mixin colorVsFont {
+//  color: #fff;
+//  font-size: 50px;
+//}
+
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 500px;
+  //@include colorVsFont;
+  table{
+    border: 1px solid black;
+    width: 300px;
+    height: 400px;
+    background-color: #312d2deb;
+    border-radius: 10px;
+    .display{
+      background-color: #0a0d10;
+      height: 80px;
+      padding: 0px;
+      margin-top: -10px;
+      border-radius: 10px;
+      position: relative;
+        p{
+          font-size: 20px;
+          font-weight: bold;
+          position: absolute;
+          color: white;
+          bottom: 0px;
+          right: 20px;
+        }
+      }
+    tr{
+      input{
+        width: 60px;
+        height: 50px;
+        border-radius: 10px;
+        color: white;
+        font-size: 20px;
+        font-weight: bold;
+        background-color: #6E6E6E;
+        }
+      .btnyellow{
+        background-color: #d69643!important;
+      }
+
+    }
+    .btnzero{
+      width: 90%;
+      height: 50px;
+    }
+  }
+
+}
+</style>
